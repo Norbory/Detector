479d4bd2781c1781aa34957f739d226acb64b006
Angelo M <65518034+Norbory@users.noreply.github.com>
2022-07-11 17:44:25 -0500

    Agregue alerta y comentarios

--- Caritas/Caras.py
+++ Caritas/Caras.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 import pickle
+import datetime
+from tkinter import *
 
 face_cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_alt2.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
@@ -12,7 +14,10 @@ with open('labels.pickle', 'rb') as f:
     labels = {v:k for k,v in og_labels.items()}
 
 cap = cv2.VideoCapture(0)
-
+#Inicializa variables a usar
+cesar=0
+pedro=0
+i = 0
 while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -20,11 +25,38 @@ while(True):
     for (x,y,w,h) in faces:
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
-
         id_,conf = recognizer.predict(roi_gray)
-        if conf >= 80:  #and conf <= 85:
+        
+        if conf >= 85:  #and conf <= 85:
             print(id_)
             print(labels[id_])
+        #Inicia cuenta de tomas de personas
+            i+=1
+        #Aumentar las variables para cada persona que puede soltar un valor
+            if id_==1:
+                cesar+=1
+            elif id_==2:
+                pedro+=1
+        #Numero de tomas para encontrar a la persona que acaba de llegar y soltar la alerta en forma de Boton porque no tengo excel aun
+            if i>=30:
+                now =datetime.datetime.now()
+                current_time = now.strftime('%H:%M:%S')
+                root = Tk()             
+                root.geometry('200x200')
+        #Comparacion de valores para hallar la mayor coincidencia
+                if cesar > pedro:
+                    btn = Button(root, text = "Cesar llego a las " +  current_time, bd = '5',command = root.destroy)
+                    btn.pack(side = 'top')
+                    root.mainloop()
+                else:
+                    btn = Button(root, text = "Alguien llego a las " +  current_time, bd = '5',command = root.destroy)
+                    btn.pack(side = 'top')
+                    root.mainloop()
+        #Reset a los valores para las siguientes lecturas            
+                i=0
+                cesar=0
+                pedro=0
+
             font = cv2.FONT_HERSHEY_SIMPLEX
             name = labels[id_]
             color = (255,255,255)
